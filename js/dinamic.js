c1b5d5771e613d53e7b8db218753e5da85ff1251
Rama <rramiro.o@hotmail.com>
2015-04-24 14:53:01 -0300

    Agregada funcionalidad de buscador basica
    Se agrego la funcionalidad de buscar materias por nombre. Funciona con
    un array de materias, y hay que corregir un par de cosas.

---
+++ js/dinamic.js
@@ -0,0 +1,24 @@
+//Al escribir en el buscador
+$("#search").on('input', function() {
+	var match = $(this).val().toLowerCase();
+	var materias = ["Matematica", "Mnemotecnia", "Quimica", "Fisica", "Biologia"];
+	$("#materias ul.materia").html("");
+	
+	//Recorrer el listado de materias
+	materias.forEach(function(i){
+		
+		//Y fijarse si alguna coincide con lo que se busca
+		//FIJARSE POR QUE AGARRA LA M COMO UNA Q!!!
+		if (i.toLowerCase().match(new RegExp(match))){
+			
+			//Si coincide, se muestra.
+			MostrarMateria(i);
+		}
+	});
+});
+
+function MostrarMateria(materia){
+	$("#materias ul.materia").append(
+	"<a href='materia.html'><li class='materia'><h3>"+ materia +"</h3><h4>94.05</h4></li></a>"
+	)
+}
