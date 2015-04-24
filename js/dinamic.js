09b997a1862079e01dbf613aa46686101b522c2a
Rama <rramiro.o@hotmail.com>
2015-04-24 19:43:04 -0300

    Corregido JS y Corregido HTML
    Ahora las materias cargan directo de JS. Se borraron las materias
    hardcodeadeas en el HTML. Se corrigieron detalles del CSS con respecto a
    los botones de la clase del dia.

--- js/dinamic.js
+++ js/dinamic.js
@@ -1,28 +1,40 @@
+var materias = ["Matematica", "Mnemotecnia", "Quimica", "Fisica", "Biologia", "Sistemas", "Filosofia", "Locucion", "Sismografia", "Laboratorio", "Programacion"];
+var fechas = ["Lunes 21/3", "Viernes 18/3", "Jueves 17/3"]
+BuscarMaterias("");
+SepararFechas();
 
-//***********************************//
-//****** BUSCADOR DE MATERIAS *******//
-//***********************************//
-
-//Al escribir en el buscador
+//Buscador de materias
 $("#search").on('input', function() {
-	var match = $(this).val().toLowerCase();
-	var materias = ["Matematica", "Mnemotecnia", "Quimica", "Fisica", "Biologia", "Sistemas", "Filosofia"];
+	var query = $(this).val().toLowerCase();
+	BuscarMaterias(query);
+});
+
+function MostrarMateria(materia){
+	$("#materias ul.materia").append(
+	"<a href='materia.html'><li class='materia'><h3>" + materia + "</h3><h4>94.05</h4></li></a>"
+	)
+}
+
+function MostrarFecha(){
+}
+
+function BuscarMaterias(query){
 	$("#materias ul.materia").html("");
 	
 	//Recorrer el listado de materias
 	materias.forEach(function(i){
 		
 		//Y fijarse si alguna coincide con lo que se busca
-		if (i.toLowerCase().match(new RegExp("^" + match))){
+		if (i.toLowerCase().match(new RegExp("^" + query))){
 			
 			//Si coincide, se muestra.
 			MostrarMateria(i);
 		}
 	});
-});
+}
 
-function MostrarMateria(materia){
-	$("#materias ul.materia").append(
-	"<a href='materia.html'><li class='materia'><h3>"+ materia +"</h3><h4>94.05</h4></li></a>"
-	)
+function SepararFechas(){
+	$("#timeline ul li.unidate").each(function (i){
+		$(this).css('right',(i*150)+'px');
+	})
 }
