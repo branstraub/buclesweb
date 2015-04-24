a68a3c3b58fe9e06e3a0200acdcf93842246dc55
Rama <rramiro.o@hotmail.com>
2015-04-24 15:03:46 -0300

    Arreglo de buscador
    Arreglado el buscador.

--- js/dinamic.js
+++ js/dinamic.js
@@ -1,15 +1,19 @@
+
+//***********************************//
+//****** BUSCADOR DE MATERIAS *******//
+//***********************************//
+
 //Al escribir en el buscador
 $("#search").on('input', function() {
 	var match = $(this).val().toLowerCase();
-	var materias = ["Matematica", "Mnemotecnia", "Quimica", "Fisica", "Biologia"];
+	var materias = ["Matematica", "Mnemotecnia", "Quimica", "Fisica", "Biologia", "Sistemas", "Filosofia"];
 	$("#materias ul.materia").html("");
 	
 	//Recorrer el listado de materias
 	materias.forEach(function(i){
 		
 		//Y fijarse si alguna coincide con lo que se busca
-		//FIJARSE POR QUE AGARRA LA M COMO UNA Q!!!
-		if (i.toLowerCase().match(new RegExp(match))){
+		if (i.toLowerCase().match(new RegExp("^" + match))){
 			
 			//Si coincide, se muestra.
 			MostrarMateria(i);
